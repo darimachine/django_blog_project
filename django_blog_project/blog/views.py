b699652cbd0a3bdf1889f686e843e6038aa3076c
Dari <serhi1334@gmail.com>
2022-11-17 12:53:10 +0200

    added templates html files and more

--- django_blog_project/blog/views.py
+++ django_blog_project/blog/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+posts=[
+    {
+        'author':'CoreyMS',
+        'title':'Blog Post 1',
+        'content':'First post content',
+        'date_posted':'August 27,2022'
+    },
+    {
+        'author':'Serhan',
+        'title':'Blog Post 2',
+        'content':'Second post content',
+        'date_posted':'August 28,2022',
+    }
+]
+
+
 def home(request):
-    return HttpResponse("<h1>Blog Home</h1>")
+    context={
+        'posts':posts,
+    }
+    return render(request,'blog/index.html',context)
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request,'blog/about.html',{'title':'About'})
 
