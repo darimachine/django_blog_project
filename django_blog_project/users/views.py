d97ec17acecf98dde5c73fb8a68a06825bc68962
Dari <serhi1334@gmail.com>
2022-11-24 01:23:05 +0200

    added update button to the profile page

--- django_blog_project/users/views.py
+++ django_blog_project/users/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm,ProfileUpdateForm,UserUpdateForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 # Create your views here.
 def register(request):
     if request.method == "POST":
@@ -17,4 +18,28 @@ def register(request):
     return render(request,'users/register.html',{'form':form})
 @login_required
 def profile(request):
-    return render(request,'users/profile.html')
+
+
+    if request.method == "POST":
+        u_form = UserUpdateForm(request.POST,instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,request.FILES,instance=request.user.profile)
+        if u_form.is_valid():
+            u_form.save()
+        if p_form.is_valid():
+            p_form.save()
+        if u_form.is_valid() or p_form.is_valid():
+            username = u_form.cleaned_data.get('username')
+            messages.success(request, f"Your Account has been updated! {username}")
+            return redirect('/profile')
+        else:
+            u_form = UserUpdateForm(instance=request.user)
+            p_form = ProfileUpdateForm(instance=request.user.profile)
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+
+    return render(request,'users/profile.html',context=context)
